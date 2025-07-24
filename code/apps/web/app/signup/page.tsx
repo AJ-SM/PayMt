d6c9638ba7ae1b155da4a55156b973c655305339
AJ-SM <anujsidam1108@gmail.com>
2025-07-24 10:38:59 +0530

    add main files

--- code/apps/web/app/signup/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Signup(){
-    return( 
-        <div className="flex justify-center items-center w-screen h-screen">
-            <div className="">Sing</div>
-            
-        </div>
-    )
-}
--- code/packages/ui/postcss.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = require("@Anuj/configs/tailwind/postcss.config")
