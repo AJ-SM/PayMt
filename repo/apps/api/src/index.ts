e422b4fca0793142460d81dc7c547def3b2993bf
AJ-SM <anujsidam1108@gmail.com>
2025-07-29 20:27:37 +0530

    added-be

--- repo/apps/api/src/index.ts
+++ repo/apps/api/src/index.ts
@@ -1,9 +1,39 @@
 import express from "express";
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+import { Request,Response,NextFunction } from "express";
 import {PrismaClient} from "../../../packages/database/generated/prisma"
 
+
+
+const JWT_SECERET = "anujsidam"
 const client = new PrismaClient;
 const app = express();
+
+function CreateToken(id:number): string{
+    const s_id = id.toString()
+    const token = jwt.sign(s_id,JWT_SECERET)
+    return token    
+}
+
+async function  UserAuth(req:Request,res:Response,next:NextFunction){
+    const token:any = req.headers['token']
+    let dd = Number(jwt.verify(token,JWT_SECERET))
+    
+    
+    const data = await client.user.findUnique({
+
+            where:{id:dd},
+        })
+    if(data){
+        next();
+    }else{
+        res.send("Noo Account Found Sorry !!!! ")
+    }
+}
+
+
+
 
 app.use(express.json());
 app.get('/',(req,res)=>{
@@ -13,24 +43,46 @@ app.get('/',(req,res)=>{
 app.post('/signup', async (req,res)=>{
     const name:string = req.body.username;
     const lastname:string = req.body.lastname; 
-    const password:string = req.body.password; 
+    let password = req.body.password; 
+    password = (await bcrypt.hash(password,10)).toString()
    
     await client.user.create({
         data:{
             name:name,
             lastname:lastname,
             password:password,
+            Balance:1000,
         }
     })
     res.send(`User Created With username ${name} !!! `)
-
 })
 
 
-app.post('/signin',(req,res)=>{
-    let name:string = req.body.username;
-    const password = req.body.password;
-    res.send(`Hello ${name}`)
+app.post('/signin',async (req,res)=>{
+    const name = req.body.username;
+    const lastname:string = req.body.lastname; 
+    const password:string = req.body.password; 
+    const users = await client.user.findUnique({
+        where :{ password },
+    })
+    if(!users){
+        res.send("User Not Found !!! ")
+    }
+    else{
+        const token:string = CreateToken(users.id)
+        res.send(token)
+    }
+
+})
+app.post('/account',UserAuth,(req,res)=>{
+    const Balance = req.body.Balance
+    
+    
+})
+
+
+app.post('/wire',UserAuth,(req,res)=>{
+
 })
 
 
