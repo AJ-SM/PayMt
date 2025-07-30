5e023f131f1423815ba2327f413796e404317bfb
AJ-SM <anujsidam1108@gmail.com>
2025-07-31 01:20:26 +0530

    added tranxaction

--- repo/apps/api/src/index.ts
+++ repo/apps/api/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt"
+
 import { Request,Response,NextFunction } from "express";
 import {PrismaClient} from "../../../packages/database/generated/prisma"
 
@@ -26,6 +26,7 @@ async function  UserAuth(req:Request,res:Response,next:NextFunction){
             where:{id:dd},
         })
     if(data){
+        
         next();
     }else{
         res.send("Noo Account Found Sorry !!!! ")
@@ -42,16 +43,17 @@ app.get('/',(req,res)=>{
 
 app.post('/signup', async (req,res)=>{
     const name:string = req.body.username;
-    const lastname:string = req.body.lastname; 
+    const lastname:string = req.body.lastname;  
+    const balance = Number(req.body.balance)
     let password = req.body.password; 
-    password = (await bcrypt.hash(password,10)).toString()
+    password.toString()
    
     await client.user.create({
         data:{
             name:name,
             lastname:lastname,
             password:password,
-            Balance:1000,
+            Balance:balance,
         }
     })
     res.send(`User Created With username ${name} !!! `)
@@ -74,17 +76,83 @@ app.post('/signin',async (req,res)=>{
     }
 
 })
-app.post('/account',UserAuth,(req,res)=>{
-    const Balance = req.body.Balance
+app.post('/account',UserAuth,async (req,res)=>{
+    const token:any = req.headers['token']
+    let dd = Number(jwt.verify(token,JWT_SECERET))
+    const data = await client.user.findUnique({
+        where:{id:dd},
+    })
+    res.send(data?.Balance)
+
+    
+   
     
     
 })
 
 
-app.post('/wire',UserAuth,(req,res)=>{
+app.post('/send', UserAuth, async (req, res) => {
+  try {
+      //@ts-ignore
+      const amount = parseFloat(req.query.amount); 
+    const uid:number = Number(req.query.id)
+    const token:any = req.headers['token'];
+    const dd = Number(jwt.verify(token,JWT_SECERET))
+    const userId= dd;
+    const user = await client.user.findUnique({
+        where:{id:dd}, select: { Balance: true },
+    })
 
-})
+    const user2 = await client.user.findUnique({
+        where:{id:uid}, select: { Balance: true },
+    })
+    if(!user2){
+        return res.status(404).json({error:"Reciver Id is not correct / Reciver NOt found!!!"})
+    }
 
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    //@ts-ignore
+    if (user.Balance < amount) {
+      return res.status(400).json({ error: "Insufficient balance" });
+    }
+
+const updatedUsers = await client.$transaction(async (tx) => {
+  const sender = await tx.user.update({
+    where: { id: userId },
+    data: {
+      Balance: {
+        decrement: amount,
+      },
+    },
+  });
+
+  const receiver = await tx.user.update({
+    where: { id: uid },
+    data: {
+      Balance: {
+        increment: amount,
+      },
+    },
+  });
+
+  return { sender, receiver };
+});
+
+
+    res.status(200).json({
+      message: "Transaction successful",
+      Sender : updatedUsers.sender.Balance,
+      Reciver :  updatedUsers.receiver.Balance,
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 
 
